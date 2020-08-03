9bacff5f89ead25075486ce3397308a6a5e8e469
Stephen Bradshaw <stephen.j.bradshaw@gmail.com>
2020-08-03 16:34:13 +0100

    Partially write formatDates utility function

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,10 +2,36 @@ const {
   formatDates,
   makeRefObj,
   formatComments,
-} = require('../db/utils/utils');
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  test("returns an empty array when passed an empty array", () => {
+    expect(formatDates([])).toEqual([]);
+  });
+  test("formats a single article object", () => {
+    const article = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+    ];
+    const formattedArticle = formatDates(article);
+    expect(formattedArticle[0]).toEqual(
+      expect.objectContaining({
+        ...article[0],
+        created_at: expect.any(Date),
+      })
+    );
+    expect(formattedArticle[0].created_at.toISOString()).toBe(
+      "2018-11-15T12:21:54.171Z"
+    );
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,9 @@
-exports.formatDates = list => {};
+exports.formatDates = (array) => {
+  if (array.length == 0) return [];
+  array[0].created_at = new Date(array[0].created_at);
+  return array;
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = (array) => {};
 
 exports.formatComments = (comments, articleRef) => {};
