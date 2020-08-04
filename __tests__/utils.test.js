9512dacccc2b0c33d884712ce6e68a371e9845fb
Stephen Bradshaw <stephen.j.bradshaw@gmail.com>
2020-08-04 12:25:48 +0100

    Complete formatComments utility function

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -214,27 +214,271 @@ describe("makeRefObj", () => {
 });
 
 describe("formatComments", () => {
-  test.only("returns an empty array when passed an empty array", () => {
+  test("returns an empty array when passed an empty array", () => {
     expect(formatComments([])).toEqual([]);
   });
-  test.only("returns a single-object array with renamed created_by key when passed a single-object array", () => {
-    const comment = {
-      created_by: "icellusedkars",
-    };
-    expect(formatComment(comment)).toEqual({
-      author: "icellusedkars",
-    });
+  test("returns a single-object array with renamed created_by key when passed a single-object array", () => {
+    const comment = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+    ];
+    const formattedComment = formatComments(comment);
+    expect(formattedComment[0]).toHaveProperty("author", "butter_bridge");
+    expect(formattedComment[0]).not.toHaveProperty("created_by");
+  });
+  test("returns a single-object array with formatted article_id", () => {
+    const article = [
+      {
+        article_id: 9,
+        title: "They're not exactly dogs, are they?",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "Well? Think about it.",
+        created_at: 533132514171,
+      },
+    ];
+    const lookup = makeRefObj(article, "title", "article_id");
+    const comment = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+    ];
+    const formattedComment = formatComments(comment, lookup);
+    expect(formattedComment[0]).toHaveProperty("article_id", 9);
+    expect(formattedComment[0]).not.toHaveProperty("belongs_to");
+  });
+  test("returns a single-object array with formatted created_at property", () => {
+    const article = [
+      {
+        article_id: 9,
+        title: "They're not exactly dogs, are they?",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "Well? Think about it.",
+        created_at: 533132514171,
+      },
+    ];
+    const lookup = makeRefObj(article, "title", "article_id");
+    const comment = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389,
+      },
+    ];
+    const formattedComment = formatComments(comment, lookup);
+    expect(formattedComment[0].created_at).toEqual(expect.any(Date));
+    expect(formattedComment[0].created_at.toISOString()).toBe(
+      "2017-11-22T12:36:03.389Z"
+    );
   });
   test("returns a correctly-formatted multi-object array when passed a multi-object array", () => {
-    //
-  });
-  test("unformatted keys persist in the returned object", () => {
-    //
+    const articles = [
+      {
+        article_id: 6,
+        title: "A",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "Delicious tin of cat food",
+      },
+      {
+        article_id: 9,
+        title: "They're not exactly dogs, are they?",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "Well? Think about it.",
+      },
+      {
+        article_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        votes: 100,
+      },
+    ];
+    const lookup = makeRefObj(articles, "title", "article_id");
+    const comments = [
+      {
+        body: "This is a bad article name",
+        belongs_to: "A",
+        created_by: "butter_bridge",
+        votes: 1,
+        created_at: 1038314163389,
+      },
+      {
+        body: "The owls are not what they seem.",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "icellusedkars",
+        votes: 20,
+        created_at: 1006778163389,
+      },
+      {
+        body: "This morning, I showered for nine minutes.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 975242163389,
+      },
+    ];
+    const formattedComments = formatComments(comments, lookup);
+    formattedComments.forEach((comment) => {
+      expect(comment).toEqual(
+        expect.objectContaining({
+          body: expect.any(String),
+          article_id: expect.any(Number),
+          author: expect.any(String),
+          votes: expect.any(Number),
+          created_at: expect.any(Date),
+        }),
+        expect.not.objectContaining({
+          belongs_to: expect.any(String),
+          created_by: expect.any(String),
+        })
+      );
+    });
   });
   test("returned array and objects have new references", () => {
-    //
+    const articles = [
+      {
+        article_id: 6,
+        title: "A",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "Delicious tin of cat food",
+      },
+      {
+        article_id: 9,
+        title: "They're not exactly dogs, are they?",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "Well? Think about it.",
+      },
+      {
+        article_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        votes: 100,
+      },
+    ];
+    const lookup = makeRefObj(articles, "title", "article_id");
+    const comments = [
+      {
+        body: "This is a bad article name",
+        belongs_to: "A",
+        created_by: "butter_bridge",
+        votes: 1,
+        created_at: 1038314163389,
+      },
+      {
+        body: "The owls are not what they seem.",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "icellusedkars",
+        votes: 20,
+        created_at: 1006778163389,
+      },
+      {
+        body: "This morning, I showered for nine minutes.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 975242163389,
+      },
+    ];
+    const formattedComments = formatComments(comments, lookup);
+    expect(formattedComments).not.toBe(comments);
+    formattedComments.forEach((comment, index) => {
+      expect(comment).not.toBe(comments[index]);
+    });
   });
   test("input is not mutated", () => {
-    //
+    const articles = [
+      {
+        article_id: 6,
+        title: "A",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "Delicious tin of cat food",
+      },
+      {
+        article_id: 9,
+        title: "They're not exactly dogs, are they?",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "Well? Think about it.",
+      },
+      {
+        article_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        votes: 100,
+      },
+    ];
+    const lookup = makeRefObj(articles, "title", "article_id");
+    const comments = [
+      {
+        body: "This is a bad article name",
+        belongs_to: "A",
+        created_by: "butter_bridge",
+        votes: 1,
+        created_at: 1038314163389,
+      },
+      {
+        body: "The owls are not what they seem.",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "icellusedkars",
+        votes: 20,
+        created_at: 1006778163389,
+      },
+      {
+        body: "This morning, I showered for nine minutes.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 975242163389,
+      },
+    ];
+    const formattedComments = formatComments(comments, lookup);
+    expect(comments).toEqual([
+      {
+        body: "This is a bad article name",
+        belongs_to: "A",
+        created_by: "butter_bridge",
+        votes: 1,
+        created_at: 1038314163389,
+      },
+      {
+        body: "The owls are not what they seem.",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "icellusedkars",
+        votes: 20,
+        created_at: 1006778163389,
+      },
+      {
+        body: "This morning, I showered for nine minutes.",
+        belongs_to: "Living in the shadow of a great man",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 975242163389,
+      },
+    ]);
   });
 });
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -13,5 +13,20 @@ exports.makeRefObj = (array, keyTarget, valueTarget) => {
 };
 
 exports.formatComments = (comments, articleRef) => {
-  return [];
+  const formattedComments = comments.map((comment) => {
+    const newComment = { ...comment };
+
+    (newComment.author = newComment.created_by), delete newComment.created_by;
+
+    if (articleRef !== undefined) {
+      newComment.article_id = articleRef[newComment.belongs_to];
+    }
+    delete newComment.belongs_to;
+
+    newComment.created_at = new Date(newComment.created_at);
+
+    return newComment;
+  });
+
+  return formattedComments;
 };
