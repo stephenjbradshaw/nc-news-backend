e80ee1da46f64383c5fb7b553a2aa749e5e22683
Stephen Bradshaw <stephen.j.bradshaw@gmail.com>
2020-08-28 15:22:27 +0100

    Add CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const apiRouter = require("./routers/api.router");
+const cors = require("cors");
 const {
   handlePSQL400Errors,
   handlePSQL404Errors,
@@ -8,6 +9,8 @@ const {
 } = require("./errors/");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", apiRouter);
